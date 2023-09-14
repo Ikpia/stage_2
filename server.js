66d9cb03e830cc67378be621aaed00023494d80b
Ikpia <echukwuka97@gmail.com>
2023-09-14 14:13:46 -0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const connectToDb = require('./models/connectToDb');
 const routes = require('./routes/routes') 
 
